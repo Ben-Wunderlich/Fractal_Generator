37512d8ff73e8e8cdbdaf4f002a31a61d7e2eb80
Ben Wunderlich <ben.wunderlich@outlook.com>
2019-10-02 19:50:54 -0700

    Refactored code
    made it easier to understand what parts can be modified and which should be left alone

--- nuJulia.py
+++ nuJulia.py
@@ -8,7 +8,7 @@ from PIL import Image
 from win10toast import ToastNotifier    
 import sys#add command line args if argv > 1
 import math
-from math import *
+from math import sin,cos,erf
 import glob
 
 toaster = ToastNotifier()
@@ -79,21 +79,23 @@ def julia(c, width=100, height=50, max=15,xView=1.5, yView=1.3):
     arr = np.zeros((height, width, 3))
     fileName=1#so it starts
     while nameTaken(fileName):
-        fileName = "interestingResults\\{}nuJulia{}.png".format(randint(1,99),c)
+        #fileName = "interestingResults\\{}nuJulia{}.png".format(randint(1,99),c)
+        fileName = "{}nuJulia{}.png".format(randint(1,99),c)
+    
     print("""STARTING\nfileSize={}x{}, c={}
     max={}, view={}x{}""".format(width, height, c, max,xView,yView))
     print("operation started at", currTime())
     strtStore = datetime.now()
-    print("\ncomputing {}...".format(fileName[19:-4]))
+    print("\ncomputing {}...".format(fileName))
 
     for x in range(0, width):
         currX = rangeScale(x, -xView, xView, 0, width)
         for y in range(0, height):
             currY = rangeScale(y, -yView, yView, 0, height)
             arr[y,x]=juliaPixel(c, currX, currY, max)
-        if keyboard.is_pressed("alt+ctrl+caps lock"):
-            print("INTERRUPTED")
-            break
+        #if keyboard.is_pressed("alt+ctrl+caps lock"):
+        #    print("INTERRUPTED")
+        #    break
 
     arr = arr / arr.max() #normalizing data in range 0 - 255
     arr = 255 * arr
@@ -104,29 +106,30 @@ def julia(c, width=100, height=50, max=15,xView=1.5, yView=1.3):
     print("it took {}".format(timeDiff(strtStore,datetime.now())))
     #toaster.show_toast("program is done","julia {}".format(c))
 
-def weirdCondition(x,y):
-    if sin(x) > cos(y)+random.random():
-        return False
-    return True
+#START EXPERIMENTING HERE
+def getFunky(x,y):
+    #y = erf(1-erf(y))
+    #y=math.gamma(abs(y)+0.01)
+    #x = cos(2**sin(x))
+    return (x,y)
 
+#only mess with this if you are feeling bold
 def juliaPixel(c, x, y,max):
+    x0,y0 = x, y#only needed when doind mandelbrot
+    
+    expansion=1.2
     i=0
-    x0 = x
-    y0 = y
-    expansion=1
-    gen_val = 1.3
     while i<max and x**2 + y**2 < 4:
-        """xtemp = x**2 - y**2
+        #JULIA
+        xtemp = x**2 - y**2  
         y = 2*x*y + c
-        x = xtemp + c"""#JULIA
-        xtemp = x**2 - y**2 + x0
+        x = xtemp + c
+        #MANDELBROT
+        '''xtemp = x**2 - y**2 + x0 
         y = 2*x*y + y0
-        x = xtemp #MANDELBROT
-        ########### start
-        '''y = erf(1-erf(y))'''
-        #y=math.gamma(abs(y)+0.01)
-        #x = cos(x)
-        ############ end
+        x = xtemp''' 
+        
+        x,y = getFunky(x,y)
         x*=expansion
         y*=expansion
         i+=1
@@ -138,19 +141,19 @@ def juliaPixel(c, x, y,max):
     #return(0,i,255-i)# on blue
 
 def main():
-    dimensions = (360,360)
-    #dimensions=(1500,1500)
-    dimensions = (3000, 3000)
+    dimensions = (400,400)
+    #dimensions=(1000,1000)
+    #dimensions = (1000, 2000)
 
     if len(sys.argv) > 1:
         loadFromArgs()
         return
-    c=500
+    c=.25
     width=dimensions[0]
     height=dimensions[1]
-    xView=2
-    yView=2
-    max=40#smaller is brighter#45
+    xView=1#full view is 2 2, interesting view is 1 1
+    yView=1
+    max=200#smaller is brighter#45
 #pick 2 points on a rectangle, make line between them
 
     julia(c,width,height,max,xView,yView)
